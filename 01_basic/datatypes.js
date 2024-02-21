3262a1afaa2ed717e38c4f5e24d02304e9894d2e
RutujaRubde <114400139+RutujaRutbde@users.noreply.github.com>
2024-02-21 04:48:05 +0000

    container config done

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer version
+//alert(3 + 3) //we are using nodejs,not browser
+
+console.log(3+3);
+console.log("Rutuja")
+let Name = "Rutuja"
+let age = 22
+let isLoggedIn = true
+console.log(typeof "Rutuja")
+console.log(typeof null)
+console.log(typeof undefined)
