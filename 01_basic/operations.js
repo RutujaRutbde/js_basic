be350e5c982edabe1ba7ab598086a280c8345314
RutujaRubde <114400139+RutujaRutbde@users.noreply.github.com>
2024-02-25 07:36:48 +0000

    operations

---
+++ 01_basic/operations.js
@@ -0,0 +1,22 @@
+let val = 3
+let negVal = -val
+// console.log(negVal)
+// console.log(2+2)
+// console.log(5-3)
+// console.log(2*8)
+// console.log(10/2)
+// console.log(2**2)
+// console.log(10%5)
+
+// let str1 = "Hello"
+// let str2 = " Rutuja"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 3)
+// console.log(1 + "3")
+// console.log("1"+2+2)
+// console.log(1+2+"3")
+
+console.log(+true)
+console.log(+'')
