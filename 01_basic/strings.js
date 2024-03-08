3eec8dd200ea4a7509692a6dabad028aa0a81be1
RutujaRubde <114400139+RutujaRutbde@users.noreply.github.com>
2024-03-08 11:54:42 +0000

    strings

---
+++ 01_basic/strings.js
@@ -0,0 +1,15 @@
+const name = "Rutuja"
+const repoCount = 50
+// console.log(name + repoCount)
+// console.log(`My name is ${name} and my Repocount is ${repoCount}`)
+const gameName = new String("Rutuja R")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf("u"))
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-7,4)
